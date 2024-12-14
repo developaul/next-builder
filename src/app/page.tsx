b8caac7c51fe00fa8d7a4840331979b05e3f29fa
Paul Chavez <paulchavezromero20@gmail.com>
2024-12-14 12:54:28 -0500

    Add Builder.io integration and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,37 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// Example file structure, app/[...page]/page.tsx
+// You could alternatively use src/app/[...page]/page.tsx
+import { builder } from "@builder.io/sdk";
+import { RenderBuilderContent } from "@/components/builder";
 
-export default function Home() {
+// Replace with your Public API Key
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY ?? '');
+
+interface PageProps {
+  params: {
+    page: string[];
+  };
+}
+
+export default async function Page(props: Readonly<PageProps>) {
+  const model = "page";
+  const content = await builder
+    // Get the page content from Builder with the specified options
+    .get("page", {
+      userAttributes: {
+        // Use the page path specified in the URL to fetch the content
+        urlPath: "/" + (props?.params?.page?.join("/") || ""),
+      },
+      // Set prerender to false to return JSON instead of HTML
+      prerender: false,
+      cachebust: true,
+    })
+    // Convert the result to a promise
+    .toPromise();
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      {/* Render the Builder page */}
+      <RenderBuilderContent content={content} model={model} />
+    </>
   );
 }
