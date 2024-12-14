6c12dc2013e81985b2a45c13a2d814d8839dc9ef
Paul Chavez <paulchavezromero20@gmail.com>
2024-12-14 12:43:11 -0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.NODE_ENV === "production" ? '/next-builder' : undefined;
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true
+  },
+  basePath
 };
 
 export default nextConfig;
