6777cd4911da74f77d627c83540f26516eb882a0
Paul Chavez <paulchavezromero20@gmail.com>
2024-12-14 15:32:56 -0500

    Enable cachebust option in page configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ export default async function Page(props: Readonly<PageProps>) {
       },
       // Set prerender to false to return JSON instead of HTML
       prerender: false,
+      cachebust: true,
     })
     // Convert the result to a promise
     .toPromise();
